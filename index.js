3e6da0f7d023557c60e30dea346d101b074a40c2
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-07-10 12:10:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port= process.env.PORT || 4050;
+
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('our-restaurant-server is running');
+
+});
+
+app.listen(port, () => {
+  console.log(`our-restaurant-server is running on port ${port}`);
+});
