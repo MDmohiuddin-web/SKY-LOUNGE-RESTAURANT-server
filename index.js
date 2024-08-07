fc7aa45c2d9ea81e8888a70e42f2195f03727b17
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-08-08 00:26:45 +0600

    first commit

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 9585;     
+const port = process.env.PORT || 9585;
 
 // middleware
-  
-app.use(cors()); 
+
+app.use(cors());
 app.use(express.json());
 
 // DATA BASE CONNECTION CODE
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.BD_PASS}@cluster0.cg8xo0z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,7 +19,7 @@ const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
-    deprecationErrors: true,      
+    deprecationErrors: true,
   },
 });
 
@@ -28,6 +28,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const MenuCollection = client.db("our-restaurant").collection("menu");
+    const usersCollection = client.db("our-restaurant").collection("users");
     const ReviewCollection = client.db("our-restaurant").collection("reviews");
     const CardCollection = client.db("our-restaurant").collection("Cards");
 
@@ -37,27 +38,58 @@ async function run() {
       res.send(result);
     });
 
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // users collections api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      //insert email if user does not exist
+      //you can do this many ways (1:email unique, 2: upsert, 3: simple checking,)
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null }); 
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    // users collections for delete users
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get("/reviews", async (req, res) => {
       const result = await ReviewCollection.find().toArray();
       res.send(result);
-    });
+    });  
 
     // cards collections
     app.get("/cards", async (req, res) => {
-       const email = req.query.email;
-       const query = { email: email };
+      const email = req.query.email;
+      const query = { email: email };
       const result = await CardCollection.find(query).toArray();
       res.send(result);
-       
-    });
-    
-    app.post("/cards", async (req, res) => {  
-      const result = await CardCollection.insertOne(req.body);   
-      res.send(result);  
-    
     });
 
-    // Send a ping to confirm a successful connection 
+    app.post("/cards", async (req, res) => {
+      const result = await CardCollection.insertOne(req.body);
+      res.send(result);
+    });
+    app.delete("/cards/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await CardCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
@@ -79,7 +111,7 @@ app.listen(port, () => {
 
 /**
  * ----------------------
- * naming convention  
+ * naming convention
  * ----------------------
  * app.get('/user')
  * app.get('/user/:id')
